2b778006b6cc84808903a9686be5265f0746a11f
yellowcatdotcom <valentinagoncarenko975@gmail.com>
2023-07-06 20:17:03 +0300

    problems with meetingRoom in DB

--- siteSpider/spiderapp/urls.py
+++ siteSpider/spiderapp/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
-from .views import configuration_view
+from .views import configuration_view, department_view, employee_view, meetingRoom_view
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('configuration/', configuration_view, name='configuration'),
+    path('departments/', department_view, name='departments'),
+    path('employees/', employee_view, name='employees'),
+    path('meetingRooms/', meetingRoom_view, name='meetingRooms'),
 ]
--- siteSpider/spiderapp/views.py
+++ siteSpider/spiderapp/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
-from .models import Configuration
+from .models import Configuration, Department, Employee, MeetingRoom
 
 
 def post_list(request):
     return render(request, 'spiderapp/post_list.html', {})
 
+
 def configuration_view(request):
-    configuration=Configuration.objects.get()
-    return render(request,'spiderapp/configuration.html', {'configuration': configuration})
+    configuration = Configuration.objects.get()
+    return render(request, 'spiderapp/configuration.html', {'configuration': configuration})
+
+
+def department_view(request):
+    departments = Department.objects.all()
+    return render(request, 'spiderapp/department.html', {'departments': departments})
+
+
+def employee_view(request):
+    employees = Employee.objects.all()
+    return render(request, 'spiderapp/employee.html', {'employees': employees})
+
+
+def meetingRoom_view(request):
+    meetingRooms = MeetingRoom.objects.all()
+    return render(request, 'spiderapp/meetingRoom.html', {'meetingRooms': meetingRooms})
